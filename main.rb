dc6cdf490e2714b94f1a745b7bba91242e0ba2c0
Megan Lee <wylee14@gmail.com>
2018-04-19 14:11:49 +1000

    Added comments and photo functionality

--- main.rb
+++ main.rb
@@ -13,6 +13,9 @@ def run_query(sql)
 end
 
 get "/" do
+  sql = "SELECT * FROM photos;"
+  @photos = run_query(sql)
+
   erb :index
 end
 
@@ -20,14 +23,19 @@ post "/" do
   erb :new
 end
 
-post "/albums" do
-  sql = "INSERT INTO albums (name, image_url, content) VALUES ('#{params[:album]}', '#{params[:image_url]}', '#{params[:content]}');"
+#Must disallow users to post blank fields later!
+post "/photos" do
+  sql = "INSERT INTO photos (name, image_url, content) VALUES ('#{params[:name]}', '#{params[:image_url]}', '#{params[:content]}');"
   run_query(sql)
   redirect to ("/")
 end
 
 delete "/" do
-  "Are you sure you want to delete this album?"
+  #sql = "INSERT INTO albums (name, image_url, content) VALUES ('#{params[:album]}', '#{params[:image_url]}', '#{params[:content]}');"
+  "DELETE FROM albums WHERE id = #{params[:id]};"
+
+  run_query(sql)
+  redirect to ("/")
 end
 
 get "/login" do
@@ -39,15 +47,41 @@ get "/login/signup" do
 end
 
 post "/login/signup" do
-  sql = "INSERT INTO users (user_name,email,password) VALUES ('#{params[:name]}', '#{params[:email]}', '#{params[:password]}');"
+  sql = "INSERT INTO users (user_name,email,password_digest) VALUES ('#{params[:name]}', '#{params[:email]}', '#{params[:password]}');"
+  #need to change password to authenticate!!!
   run_query(sql)
   redirect to ("/login")
 end
 
 post "/session" do
-  "Hello User"
+  "welcome back user"
+  #redirect to ("/")
 end
 
 delete "/session" do
   redirect to("/login")
+end
+
+#When user clicks on the photo on homepage, takes them to comment/delete pg
+get "/photos/:id" do
+  sql = "SELECT * FROM photos WHERE id= #{params[:id]};"
+  results = run_query(sql)
+  @photo = results.first
+  @comments = run_query("SELECT * FROM comments WHERE photo_id = #{params[:id]}")
+
+  erb :show
+end
+
+post "/photos/:id" do
+  sql = "INSERT INTO comments (content, photo_id) VALUES ('#{params[:content]}', '#{params[:photo_id]}');"
+  run_query(sql)
+  redirect to("/photos/#{params[:photo_id]}")
+end
+
+get "/About" do
+  erb :about
+end
+
+get "/Contact" do
+  erb :contact
 end
